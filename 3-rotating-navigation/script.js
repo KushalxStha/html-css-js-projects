ec7d04e17f217127e9011ff923d0b78f11b4b0f2
Kushal Shrestha <shresthakushal61@gmail.com>
2024-04-02 17:40:05 +0545

    Removed duplicate files

--- 3-rotating-navigation/script.js
+++
@@ -1,6 +0,0 @@
-const opens = document.querySelector('#open');
-const closes = document.querySelector('#close');
-const container = document.querySelector('.container');
-
-opens.addEventListener('click',()=>container.classList.add('show-nav'));
-closes.addEventListener('click',()=>container.classList.remove('show-nav'));
